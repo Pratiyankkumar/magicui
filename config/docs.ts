2ff7abece51b1df6bf680cb8551b2cdf86805bd8
pratiyankkumar <kumarpratiyank@gmail.com>
2025-02-11 20:43:35 +0530

    feat: add a new highlighter component

--- config/docs.ts
+++ config/docs.ts
@@ -309,6 +309,12 @@ export const docsConfig: DocsConfig = {
           items: [],
           label: "",
         },
+        {
+          title: "Highlighter",
+          href: "/docs/components/highlighter",
+          items: [],
+          label: "New",
+        },
       ],
     },
     {
